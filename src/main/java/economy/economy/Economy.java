e12dd6fe68bb9c3e3255b76fe8c36fa71561e233
Rayen <rayenrathmann@gmail.com>
2023-03-13 16:17:17 +0100

    Created project Economy.

---
+++ src/main/java/economy/economy/Economy.java
@@ -0,0 +1,16 @@
+package economy.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Economy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
