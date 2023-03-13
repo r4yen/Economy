5d10427b9a09a6a16938953362f436240b4c6625
Rayen <rayenrathmann@gmail.com>
2023-03-13 16:20:54 +0100

    Created project Economy.

--- src/main/java/economy/economy/Economy.java
+++ src/main/java/economy/economy/Economy.java
@@ -1,16 +1,19 @@
 package economy.economy;
 
+import economy.economy.commands.MoneyCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Economy extends JavaPlugin {
 
+    public static Economy instance = null;
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        this.getCommand("money").setExecutor(new MoneyCommand());
     }
 
-    @Override
-    public void onDisable() {
-
+    public static Boolean CommandExecutor() {
+        return true;
     }
 }
---
+++ src/main/java/economy/economy/commands/MoneyCommand.java
@@ -0,0 +1,14 @@
+package economy.economy.commands;
+
+import economy.economy.Economy;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class MoneyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("Coming soon.");
+        return Economy.CommandExecutor();
+    }
+}
