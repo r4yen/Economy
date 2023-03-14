c4f0dd2def33f0019619f6287773f237448152e4
rayen <52254539+r4yen@users.noreply.github.com>
2023-03-14 11:52:18 +0100

    Update Economy.java

--- src/main/java/economy/economy/Economy.java
+++ src/main/java/economy/economy/Economy.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Economy extends JavaPlugin {
 
+    public static List<UUID> players = new ArrayList<>();
     public static Economy instance = null;
 
     @Override
@@ -13,7 +14,29 @@ public final class Economy extends JavaPlugin {
         this.getCommand("money").setExecutor(new MoneyCommand());
     }
 
-    public static Boolean CommandExecutor() {
-        return true;
+    public static int getMoney(UUID player) {
+        if(!players.contains(player)) {
+            players.add(player);
+        }
+
+        return instance.getConfig().getInt("money." + player);
+    }
+
+    public static void addMoney(UUID player, int amount) {
+        if(!players.contains(player)) {
+            players.add(player);
+        }
+
+        instance.getConfig().set("money." + player, getMoney(player) + amount);
+        instance.saveConfig();
+    }
+
+    public static void setMoney(UUID player, int amount) {
+        if(!players.contains(player)) {
+            players.add(player);
+        }
+
+        instance.getConfig().set("money." + player, amount);
+        instance.saveConfig();
     }
 }
