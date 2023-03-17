35180806e737f6900277a9f8066a80a596713f70
rayen <52254539+r4yen@users.noreply.github.com>
2023-03-17 11:00:37 +0100

    Update Messages.java

--- src/main/java/economy/economy/Messages.java
+++ src/main/java/economy/economy/Messages.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 
 public class Messages {
     public static String getMessage(String string, HashMap<String, String> replacements) {
+        String message = null,
+
+        for(String s: replacements.
+
         return null;
     }
 }
